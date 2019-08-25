f9552689d650af02ac05f6581a81eec585fe33ad
Jonathan <jonathanxu25@gmail.com>
2019-08-25 17:14:34 -0400

    basic ui setup

---
+++ app/src/main/java/com/jonathanxu/hacklodge/ui/gallery/GalleryViewModel.kt
@@ -0,0 +1,13 @@
+package com.jonathanxu.hacklodge.ui.gallery
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class GalleryViewModel : ViewModel() {
+
+    private val _text = MutableLiveData<String>().apply {
+        value = "This is gallery Fragment"
+    }
+    val text: LiveData<String> = _text
+}
