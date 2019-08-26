da04c1617fcda5f9cd78d73489aed3057083b166
Misha Larionov <MishaLarionov@users.noreply.github.com>
2019-08-26 17:48:35 -0400

    Non-functional WiFi Direct code

--- app/src/main/java/com/jonathanxu/hacklodge/ui/share/ShareFragment.kt
+++ app/src/main/java/com/jonathanxu/hacklodge/ui/share/ShareFragment.kt
@@ -1,8 +1,15 @@
 package com.jonathanxu.hacklodge.ui.share
 
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
 import android.content.IntentFilter
+import android.net.wifi.p2p.WifiP2pDevice
+import android.net.wifi.p2p.WifiP2pDeviceList
 import android.net.wifi.p2p.WifiP2pManager
 import android.os.Bundle
+import android.os.Looper
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -13,10 +20,70 @@ import androidx.lifecycle.ViewModelProviders
 import com.jonathanxu.hacklodge.R
 import kotlinx.android.synthetic.main.fragment_share.*
 
-class ShareFragment : Fragment() {
+class ShareFragment : Fragment(), WifiP2pManager.PeerListListener {
 
     private lateinit var shareViewModel: ShareViewModel
     private val intentFilter = IntentFilter()
+    private lateinit var channel: WifiP2pManager.Channel
+    private lateinit var manager: WifiP2pManager
+    private val peers = mutableListOf<WifiP2pDevice>()
+    private val TAG = "ShareFragment"
+
+    private val peerListListener = WifiP2pManager.PeerListListener { peerList ->
+        val refreshedPeers = peerList.deviceList
+        if (refreshedPeers != peers) {
+            peers.clear()
+            peers.addAll(refreshedPeers)
+
+            // If an AdapterView is backed by this data, notify it
+            // of the change. For instance, if you have a ListView of
+            // available peers, trigger an update.
+            // (listAdapter as WiFiPeerListAdapter).notifyDataSetChanged()
+
+            // Perform any other updates needed based on the new list of
+            // peers connected to the Wi-Fi P2P network.
+        }
+
+        if (peers.isEmpty()) {
+            Log.d(TAG, "No devices found")
+            return@PeerListListener
+        }
+    }
+
+    private lateinit var receiver: BroadcastReceiver
+
+    object Receiver : BroadcastReceiver() {
+
+        private var isWifiP2pEnabled: Boolean = false
+
+        override fun onReceive(context: Context, intent: Intent) {
+            when (intent.action) {
+                WifiP2pManager.WIFI_P2P_STATE_CHANGED_ACTION -> {
+                    // Determine if Wifi P2P mode is enabled or not, alert
+                    // the Activity.
+                    val state = intent.getIntExtra(WifiP2pManager.EXTRA_WIFI_STATE, -1)
+                    this.isWifiP2pEnabled = state == WifiP2pManager.WIFI_P2P_STATE_ENABLED
+                }
+                WifiP2pManager.WIFI_P2P_PEERS_CHANGED_ACTION -> {
+
+                    // The peer list has changed! We should probably do something about
+                    // that.
+
+                }
+                WifiP2pManager.WIFI_P2P_CONNECTION_CHANGED_ACTION -> {
+
+                    // Connection state changed! We should probably do something about
+                    // that.
+
+                }
+                WifiP2pManager.WIFI_P2P_THIS_DEVICE_CHANGED_ACTION -> {
+
+                    // Idk what this does lmao
+
+                }
+            }
+        }
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -30,6 +97,11 @@ class ShareFragment : Fragment() {
         shareViewModel.text.observe(this, Observer {
             textView.text = it
         })
+
+        return root
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         // Indicates a change in the Wi-Fi P2P status.
@@ -44,16 +116,37 @@ class ShareFragment : Fragment() {
         // Indicates this device's details have changed.
         intentFilter.addAction(WifiP2pManager.WIFI_P2P_THIS_DEVICE_CHANGED_ACTION)
 
-        return root
-    }
+        Log.d("SHARE", "Added WifiP2pManager actions to intentFilter")
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
+        manager = this.context?.getSystemService(Context.WIFI_P2P_SERVICE) as WifiP2pManager
+        channel = manager.initialize(this.context, Looper.getMainLooper(), null)
 
-        print("this code is the big wack")
-
-        val shareText: TextView? = view?.findViewById<TextView>(R.id.text_share)
-        shareText?.text  = "y e e e e e e e t"
+        manager.discoverPeers(channel, null) // Typically a listener goes here but idc
 
     }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        button_direct_scan.text = getString(R.string.scan_direct)
+    }
+
+    override fun onPeersAvailable(list: WifiP2pDeviceList?) {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    /** register the BroadcastReceiver with the intent values to be matched  */
+    public override fun onResume() {
+        super.onResume()
+        TODO("Need to figure out how to properly do Receiver() - Probably in a util class")
+//        receiver = Receiver(manager, channel, this)
+//        registerReceiver(receiver, intentFilter)
+    }
+
+    public override fun onPause() {
+        super.onPause()
+        TODO("As above")
+//        unregisterReceiver(receiver)
+    }
+
 }
