50567235ee3a63643eea810bd3633c6a3baaa8ea
Misha Larionov <MishaLarionov@users.noreply.github.com>
2019-08-25 23:30:05 -0400

    File writing with proper title

--- app/src/main/java/com/jonathanxu/hacklodge/PostEditorActivity.kt
+++ app/src/main/java/com/jonathanxu/hacklodge/PostEditorActivity.kt
@@ -4,8 +4,10 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.view.View
+import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_post_editor.*
 import java.io.File
+import java.util.*
 
 
 class PostEditorActivity : AppCompatActivity() {
@@ -13,9 +15,20 @@ class PostEditorActivity : AppCompatActivity() {
     private fun savePost(view: View) {
         // Save the post by writing to file
         Log.d("SAVE", "Writing file to private storage")
-        // Todo post name
-        val file = File(filesDir, "post_hard.md")
-        file.writeText("This a file")
+        // Get the data from the editor
+        // Todo the actual post body
+        val title = et_title.text.toString().trim()
+        et_title.text.clear()
+        val location = et_location.text.toString().trim()
+        et_location.text.clear()
+        // Make file title lower_case
+        val fileTitle = title.replace(" ", "_").toLowerCase(Locale.ROOT)
+        val file = File(filesDir, "$fileTitle.md")
+        // Write the file todo construct the string before passing to writeText
+        // Todo consider whether to include location and such as metadata
+        file.writeText("# $title\nThis file that came from $location")
+        // Notify the user the file was saved
+        Toast.makeText(this, "Saved!", Toast.LENGTH_SHORT).show()
         Log.d("SAVE", "File written to ${file.absolutePath}")
     }
 
@@ -26,6 +39,6 @@ class PostEditorActivity : AppCompatActivity() {
 
         //Todo: Add back button
 
-        button_save_file.setOnClickListener { view -> savePost(view)}
+        button_save_file.setOnClickListener { view -> savePost(view) }
     }
 }
